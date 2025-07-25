cb8e8544c0979560b38c9117581ccb4fe489a13a
FilthyRC <ledzep.rc@gmail.com>
2025-07-25 12:59:28 +0100

    Made elements desktop scalable

---
+++ src/assets/home page/images_home.jsx
@@ -0,0 +1,9 @@
+import image1 from "./home image 1.jpg";
+import image2 from "./icon1.png";
+
+const Images = {
+  title_image: image1,
+  icon1: image2,
+};
+
+export default Images;
--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,6 +1,6 @@
 import "./home.css";
 import Navbar from "../../components/navbar/navbar";
-import image from "../../assets/home image 1.jpg";
+import Images from "../../assets/home page/images_home";
 
 function Home() {
   return (
@@ -22,7 +22,7 @@ function Home() {
           </a>
         </div>
       </div>
-      <img className="img1" src={image} />
+      <img className="img1" src={Images.title_image} />
       <div className="section-1">
         <div className="section-1-header">
           <h3 className="section-1-title">
@@ -33,6 +33,48 @@ function Home() {
             solutions tailored to your needs. we leverage cutting-edge
             technologies to deliveer software solutions that drive results.
           </p>
+        </div>
+        <div className="section-1-content">
+          <div>
+            <img src={Images.icon1} />
+            <h3>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum
+            </h3>
+          </div>
+          <div>
+            <div>
+              <img src={Images.icon1} />
+              <h3>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum
+              </h3>
+            </div>
+          </div>
+          <div>
+            <div>
+              <img src={Images.icon1} />
+              <h3>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+                sunt in culpa qui officia deserunt mollit anim id est laborum
+              </h3>
+            </div>
+          </div>
         </div>
       </div>
     </>
